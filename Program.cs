db492833498a6c8160c9c1670a6d2c684e41e62a
Robert Atkinson <bob@theatkinsons.org>
2020-03-02 17:46:44 -0800

    initial stuff

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CfxMeltPivot
+    {
+    class Program
+        {
+        public ProgramOptions ProgramOptions = new ProgramOptions();
+
+        public void DoMain(string[] args)
+            {
+            ProgramOptions.Parse(args);
+
+            // Open the output file, creating if necessary, but appending if not
+            using var fs = new FileStream(ProgramOptions.OutputFileName, FileMode.Append, FileAccess.Write, FileShare.Read);
+            }
+
+        static void Main(string[] args)
+            {
+            var program = new Program();
+            program.DoMain(args);
+            }
+        }
+    }
---
+++ ProgramOptions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Options;
+
+namespace CfxMeltPivot
+    {
+    class ProgramOptions
+        {
+        public List<String> ExtraOptions = new List<string>();
+        public string       InputFileName = null;
+        public bool         ShowUsage = false;
+        public bool         Verbose = false;
+        
+        public string OutputFileRoot = "Fluorescence";
+        public string OutputExtension = ".csv";
+        public string OutputFileName => OutputFileRoot + OutputExtension;
+
+        public IndentedTextWriter StdOut;
+        public IndentedTextWriter StdErr;
+        public string IndentString = "   ";
+
+        private OptionSet Options;
+
+        public ProgramOptions()
+            {
+            Options = new OptionSet
+                {
+                    { "f=|file=",  $"the name of the exported CFX file to parse", (string f) => InputFileName = f },
+                    { "o=|output=", $"root name of the output file (default=\"{OutputFileRoot}\"). Always of type {OutputExtension}", s => OutputFileRoot = s },
+                    { "h|help|?",  $"show this message and exit", (string h) => ShowUsage = h != null },
+                };
+
+            // In case we don't validate
+            StdOut = MakeIndentedTextWriter(Console.Out);
+            StdErr = MakeIndentedTextWriter(Console.Error);
+            }
+
+        public void Parse(string[] args)
+            {
+            try
+                {
+                ExtraOptions = Options.Parse(args);
+                if (InputFileName == null && ExtraOptions.Count > 0)
+                    {
+                    InputFileName = ExtraOptions[0];
+                    ExtraOptions.RemoveAt(0);
+                    }
+                if (ShowUsage)
+                    {
+                    Usage(null);
+                    }
+                Validate();
+                
+                StdOut = MakeIndentedTextWriter(Console.Out);
+                StdErr = MakeIndentedTextWriter(Console.Error);
+                }
+            catch (OptionException e)
+                {
+                Usage(e);
+                }
+            }
+
+        private IndentedTextWriter MakeIndentedTextWriter(TextWriter writer)
+            {
+            var result = new IndentedTextWriter(writer, IndentString);
+            result.Indent = 0;
+            return result;
+            }
+
+
+        private void Throw(string message, string optionName = null)
+            {
+            throw new OptionException(Options.MessageLocalizer($"Error: { message }"), optionName);
+            }
+
+        private void Validate()
+            {
+            if (ExtraOptions.Count > 0)
+                {
+                Throw($"{ ExtraOptions.Count } extra options given");
+                }
+            }
+
+        private void Usage(OptionException e)
+            {
+            }
+        }
+    }
